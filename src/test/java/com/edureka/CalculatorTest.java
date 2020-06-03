99ca548a1003eb24d11238501022aae5ddc63ec6
Rajgcp <rprasadp.yxe@gmail.com>
2020-06-03 03:33:10 +0000

    Coverage problem created

--- src/test/java/com/edureka/CalculatorTest.java
+++ src/test/java/com/edureka/CalculatorTest.java
@@ -51,7 +51,7 @@ public class CalculatorTest extends Mockito{
         assertTrue("Expecting Subtraction but not found",stringWriter.toString().contains("Subtraction"));
     }
 
-    @Test
+    //@Test
     public void testMulServlet() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);       
         HttpServletResponse response = mock(HttpServletResponse.class);    
@@ -72,7 +72,7 @@ public class CalculatorTest extends Mockito{
         assertTrue("Expecting Multiplication but not found",stringWriter.toString().contains("Multiplication"));
     }
     
-    @Test
+   // @Test
     public void testDivServlet() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);       
         HttpServletResponse response = mock(HttpServletResponse.class);    
@@ -93,7 +93,7 @@ public class CalculatorTest extends Mockito{
         assertTrue("Expecting Division but not found",stringWriter.toString().contains("Division"));
     }
 
-   // @Test
+   @Test
     public void testExcServlet() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);       
         HttpServletResponse response = mock(HttpServletResponse.class);    
